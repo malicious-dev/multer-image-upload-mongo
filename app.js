5197bfde5b7e417408d30f7098d3eaccdfc89250
malwareami! <46420533+malicious-dev@users.noreply.github.com>
2023-02-06 06:41:28 +0530

    file upload on mongo

---
+++ app.js
@@ -0,0 +1,101 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+const directory = "uploads";
+
+fs.readdir(directory, (err, files) => {
+  if (err) throw err;
+
+  for (const file of files) {
+    fs.unlink(path.join(directory, file), (err) => {
+      if (err) throw err;
+    });
+  }
+});
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({ storage: storage });
+
+mongoose.connect("mongodb://localhost:27017/Multertest", 
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+},
+ (err) => {
+    if (err) return console.log(err);
+    app.listen(6000, () => {
+        console.log("MongoDB Server listening on 3000");
+    });
+});
+
+app.get("/",  (req, res) => {
+    ImageModel.find({}, (err, images) => {
+      if (err) {
+          console.log(err);
+          res.status(500).send("An error occurred", err);
+      } else {
+          res.render("index", {images: images});
+          fs.readdir(directory, (err, files) => {
+            if (err) throw err;
+          
+            for (const file of files) {
+              fs.unlink(path.join(directory, file), (err) => {
+                if (err) throw err;
+              });
+            }
+          });
+      }
+    });
+  });
+
+
+const imageSchema = new mongoose.Schema({
+    image: {
+        data: Buffer,
+        contentType: String
+    }
+});
+
+const ImageModel = mongoose.model("Image", imageSchema);
+
+app.post("/uploadPhoto", upload.single("myImage"), (req, res) => {
+    const obj = {
+        img: {
+            data: fs.readFileSync(path.join(__dirname + "/uploads/" + req.file.filename)),
+            contentType: "image/png"
+        }
+    }
+    const newImage = new ImageModel({
+        image: obj.img
+    });
+
+    newImage.save((err) => {
+        err ? console.log(err) : res.redirect("/");
+    });
+});
+
+
+
+app.listen(4000, () => {
+  console.log("Server is listening on Port 5000");
+});
